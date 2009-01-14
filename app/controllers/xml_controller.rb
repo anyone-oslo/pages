e8ed3fedb73f03de62adfdc18d94259a6afcbe70
alexander <alexander@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2009-01-14 16:00:20 +0000

    pages: xml language fix

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -1,15 +1,13 @@
 class XmlController < ApplicationController
 
 	def sitemap
-		@language = params[:language] || Language.default
-		
 		@rss_feeds = Page.find( :all, :conditions => 'feed_enabled = 1 AND status = 2' ).collect{ |p| p.working_language = @language; p }
 
 		# cache everything
 		Page.find( :all )
 		
 		# grab root pages
-		@pages = Page.root_pages
+        @pages = Page.root_pages(:language => @language) 
 		render :layout => false
 	end
 
