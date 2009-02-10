46682e390cea81eb32653049ceae193fa2519069
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2009-02-10 18:49:10 +0000

    pages: jQuery love on PagesController#index

--- public/javascripts/admin/pages_controller.js
+++ public/javascripts/admin/pages_controller.js
@@ -1,8 +1,31 @@
 Admin.PagesController = {
 	init : function() {
 	},
+
 	index : function() {
+
+		// Hover actions on .page .actions
+		jQuery('.page').hover(
+			function(){
+				jQuery(this).addClass('hover');
+				jQuery(this).children('.actions').css('visibility', 'visible');
+			},
+			function(){
+				jQuery(this).removeClass('hover');
+				jQuery(this).children('.actions').css('visibility', 'hidden');
+			}
+		);
+		jQuery('.page .actions').css('visibility', 'hidden');
+
+		// Toggling of the new category input
+		window.toggleNewCategory = function(){
+			jQuery('#new-category').toggle();
+			jQuery('#new-category-button').toggle();
+		}
+		jQuery('#new-category').hide();
+
 	},
+	
 	edit : function() {
 	}
 }
