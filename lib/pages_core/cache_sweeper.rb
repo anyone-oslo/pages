1478b76cffcb7aee077e4c6c895b547ef5af618a
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2009-02-20 16:57:06 +0000

    pages: Oops

--- lib/pages_core/cache_sweeper.rb
+++ lib/pages_core/cache_sweeper.rb
@@ -33,7 +33,7 @@ module PagesCore
 					kill_patterns = self.config.patterns
 					paths = []
 					Find.find(cache_dir+"/") do |path|
-						Find.prune if file =~ Regexp.new("^#{cache_dir}/dynamic_image") # Ignore dynamic image
+						Find.prune if path =~ Regexp.new("^#{cache_dir}/dynamic_image") # Ignore dynamic image
 						file = path.gsub(Regexp.new("^#{cache_dir}"), "")
 						kill_patterns.each do |p|
 							if file =~ p && File.exist?( path )
