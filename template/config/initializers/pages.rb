df57d10a11e536cc441f07cf678769c7e14f0e84
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2009-04-28 10:04:16 +0000

    pages: Updated app template

--- template/config/initializers/pages.rb
+++ template/config/initializers/pages.rb
@@ -1,8 +1,6 @@
 # Configuration file for Pages CMS
 # Be sure to restart your web server when you modify this file.
 
-require 'acts_as_ferret'
-
 # Default language
 Language.default = Language.code_for_name("<%= @default_language %>")
 
@@ -13,25 +11,26 @@ Language.default = Language.code_for_name("<%= @default_language %>")
 # Flickr API key
 #ENV['FLICKR_API_KEY']        = ''
 
-module PagesCore
-	config :site_name,              '<%= @site_name %>'                         # Name of site
-	config :default_sender,         '<%= @mail_sender %>'                       # Default email sender
-	config :localizations,          false                                       # Set to true to enable multiple languages
-	config :text_filter,            'textile'                                   # Default text filter (markdown is available)
-	config :page_additional_images, true                                        # Enable image uploading on pages
-	config :page_files,             false                                       # Enable file uploading on pages
-	config :page_image_is_linkable, false                                       # Page image is linkable
-	config :recaptcha,              true                                        # Enable reCAPTCHA
-	config :flickr_api,             false                                       # Enable Flickr API integration
-	config :http_caching,           true
-	config :page_cache,             true                                        # Enable page caching
-	
-	# Configure the cache sweeper, add any custom paths and models
-	# CacheSweeper.config do |sweeper_config|
-	# 	sweeper_config.observe  += [Store, StoreTest, Ad]
-	# 	sweeper_config.patterns += [/^\/arkiv(.*)$/, /^\/tests(.*)$/]
-	# end
-end
+PagesCore.configure(
+	:site_name              => '<%= @site_name %>',                         # Name of site
+	:default_sender         => '<%= @mail_sender %>',                       # Default email sender
+	:localizations          => false,                                       # Set to true to enable multiple languages
+	:text_filter            => 'textile',                                   # Default text filter (markdown is available)
+	:page_additional_images => true,                                        # Enable image uploading on pages
+	:page_files             => false,                                       # Enable file uploading on pages
+	:page_image_is_linkable => false,                                       # Page image is linkable
+	:recaptcha              => false,                                       # Enable reCAPTCHA
+	:flickr_api             => false,                                       # Enable Flickr API integration
+	:http_caching           => true,
+	:page_cache             => true,                                        # Enable page caching
+	:sphinx_enabled         => true
+)	
+
+# Configure the cache sweeper, add any custom paths and models
+# PagesCore::CacheSweeper.config do |sweeper_config|
+# 	sweeper_config.observe  += [Store, StoreTest, Ad]
+# 	sweeper_config.patterns += [/^\/arkiv(.*)$/, /^\/tests(.*)$/]
+# end
 
 module Newsletter
     # Additional subscribers for newsletter
