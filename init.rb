8af2125d8557f8f59ae0b0b2d42768fe7010eaab
Inge Jørgensen <inge@manualdesign.no>
2011-01-19 18:47:16 +0100

    Monkey patch rails to default session expiry to now + 3 years

--- init.rb
+++ init.rb
@@ -13,3 +13,12 @@ end
 ENV['RECAPTCHA_PUBLIC_KEY']  = "***REMOVED***"
 ENV['RECAPTCHA_PRIVATE_KEY'] = "***REMOVED***"
 
+# Monkey patch CGI::Session::CookieStore to default session expiry 
+# to 3 years into the future.
+class CGI::Session::CookieStore
+	alias_method :original_write_cookie, :write_cookie
+    def write_cookie(options)
+		options['expires'] ||= 3.years.from_now
+		original_write_cookie(options)
+	end
+end
