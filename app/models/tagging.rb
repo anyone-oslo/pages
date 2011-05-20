528a4ab0f6a70957ff64361aaf4018acceb99371
Inge Jørgensen <inge@manualdesign.no>
2011-05-20 12:03:53 +0200

    Fiks preview-bug med saving av blanke pages

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,8 @@
 class Tagging < ActiveRecord::Base
 	belongs_to :tag
 	belongs_to :taggable, :polymorphic => true
+	
+	validates_presence_of :taggable_id, :taggable_type, :tag_id
 
 	def self.tagged_class(taggable)
 		ActiveRecord::Base.send(:class_name_of_active_record_descendant, taggable.class).to_s
