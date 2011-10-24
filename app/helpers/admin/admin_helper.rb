760050ed723fb43fccbac396d5d7d70f28a44fe1
Inge Jørgensen <inge@manualdesign.no>
2011-10-24 17:24:16 +0200

    Tag editor kan nå ta alle type objekter så lenge de har name

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,19 +1,21 @@
 module Admin::AdminHelper
 
-	def tag_editor_for(form_helper, item)
-		tags = Tag.tags_and_suggestions_for(item, :limit => 20)
+	def tag_editor_for(form_helper, item, field_name = :serialized_tags, options = {})
+		options[:tags]        ||= Tag.tags_and_suggestions_for(item, :limit => 20)
+		options[:tagged]      ||= item.tags
+		options[:placeholder] ||= 'Add tag...'
 		content_tag(:div, :class => 'tag_editor clearfix') do
-			form_helper.hidden_field(:serialized_tags, :class => 'serialized_tags') +
+			form_helper.hidden_field(field_name, :class => 'serialized_tags') +
 			content_tag(:div, :class => :tags) do
-				tags.map do |tag|
+				options[:tags].map do |tag|
 					content_tag(:span, :class => :tag) do
-						check_box_tag("tag-#{tag.id}", 1, item.tags.include?(tag)) +
+						check_box_tag("tag-#{tag.id}", 1, options[:tagged].include?(tag)) +
 						content_tag(:span, tag.name, :class => :name)
 					end
 				end.join
 			end +  
 			content_tag(:div, :class => 'add_tag_form') do
-				text_field_tag('add_tag', 'Add tag...', :class => 'add_tag') + 
+				text_field_tag('add_tag', options[:placeholder], :class => 'add_tag') + 
 				content_tag(:button, 'Add', :class => 'add_tag_button')
 			end
 		end
