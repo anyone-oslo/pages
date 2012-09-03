3cf608d5397ded67c0db10c5e53ea134b9a9d22d
Inge Jørgensen <inge@manualdesign.no>
2012-09-03 16:31:19 +0200

    Fiks for autoreloading

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@ end
 #Sass::Plugin.options[:template_location] = Rails.root.join('app', 'assets', 'stylesheets').to_s
 
 # Make the plugin reloadable
-ActiveSupport::Dependencies.load_once_paths.delete(PagesCore.plugin_root.join('lib').to_s)
-ActiveSupport::Dependencies.load_once_paths.delete(PagesCore.plugin_root.join('app', 'models').to_s)
-ActiveSupport::Dependencies.load_once_paths.delete(PagesCore.plugin_root.join('app', 'helpers').to_s)
-ActiveSupport::Dependencies.load_once_paths.delete(PagesCore.plugin_root.join('app', 'controllers').to_s)
+ActiveSupport::Dependencies.autoload_once_paths.delete(PagesCore.plugin_root.join('lib').to_s)
+ActiveSupport::Dependencies.autoload_once_paths.delete(PagesCore.plugin_root.join('app', 'models').to_s)
+ActiveSupport::Dependencies.autoload_once_paths.delete(PagesCore.plugin_root.join('app', 'helpers').to_s)
+ActiveSupport::Dependencies.autoload_once_paths.delete(PagesCore.plugin_root.join('app', 'controllers').to_s)
