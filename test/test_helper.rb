669529b6a4305f82bdd8617240a36bf6cdf4f588
Inge Jørgensen <inge@manualdesign.no>
2012-10-11 02:19:47 +0200

    Removed old cruft

--- test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-# Load the normal Rails helper. This ensures the environment is loaded
-require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
-# force these config values
-module Pages
-#  config :some_option, "some_value"
-end
