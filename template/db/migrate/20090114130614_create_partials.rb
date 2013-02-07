748ad352a6057a87b945d55defc6c2f817d65d0f
Inge Jørgensen <inge@manualdesign.no>
2013-02-07 17:08:20 +0100

    Migrations fix

--- template/db/migrate/20090114130614_create_partials.rb
+++ template/db/migrate/20090114130614_create_partials.rb
@@ -2,17 +2,11 @@
 
 class CreatePartials < ActiveRecord::Migration
   def self.up
-    
-    # Modify fragments table
     remove_column :page_fragments, :name
     remove_column :page_fragments, :description
     remove_column :page_fragments, :author_id
     rename_column :page_fragments, :slug, :name
     rename_table  :page_fragments, :partials
-    
-    # Change textbits
-    Textbit.update_all "textable_type = 'Partial'", "textable_type = 'PageFragment'"
-    
   end
 
   def self.down
@@ -21,7 +15,5 @@ class CreatePartials < ActiveRecord::Migration
     add_column :page_fragments, :name, :string
     add_column :page_fragments, :author_id, :integer
     add_column :page_fragments, :description, :text
-
-    Textbit.update_all "textable_type = 'PageFragment'", "textable_type = 'Partial'"
   end
 end
