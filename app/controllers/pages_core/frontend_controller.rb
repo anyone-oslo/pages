29ff258c701f834843072ac04f9736cd941de7eb
Inge Jørgensen <inge@manualdesign.no>
2013-02-20 15:55:09 +0100

    Set I81n locale in FrontendController from params[:language]

--- app/controllers/pages_core/frontend_controller.rb
+++ app/controllers/pages_core/frontend_controller.rb
@@ -5,6 +5,8 @@ class PagesCore::FrontendController < ApplicationController
 
   include ApplicationHelper
 
+  before_filter :set_i18n_locale
+
   # Loads @root_pages and @rss_feeds. To automatically load these in your own controllers,
   # add the following line to your controller definition:
   #
@@ -13,6 +15,16 @@ class PagesCore::FrontendController < ApplicationController
   def load_root_pages
     @root_pages = Page.root_pages( :language => @language )
     @rss_feeds = Page.find( :all, :conditions => 'feed_enabled = 1 AND status = 2' ).collect{ |p| p.working_language = @language; p }
+  end
+
+  private
+
+  def set_i18n_locale
+    if @language == 'nor'
+      I18n.locale = :nb
+    else
+      I18n.locale = :en
+    end
   end
 
 end
