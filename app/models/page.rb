90de3544cc5414ee913b1c78e0d0c92d91f09872
Inge Jørgensen <inge@manualdesign.no>
2013-05-12 16:57:50 +0200

    Cleans up Page#to_xml a bit

--- app/models/page.rb
+++ app/models/page.rb
@@ -241,9 +241,12 @@ class Page < ActiveRecord::Base
   end
 
   def to_xml(options = {})
-    default_except = [:comments_count, :byline, :delta, :last_comment_at, :image_id]
-    options[:except] = (options[:except] ? options[:except] + default_except : default_except)
+    # Always skip these
+    options[:except] = [:comments_count, :byline, :delta, :last_comment_at, :image_id] + Array(options[:except])
+
     super(options) do |xml|
+
+      # Localizations
       self.template_config.enabled_blocks.each do |localizable_name, block_options|
         xml.tag!(localizable_name.to_sym) do |field|
           self.locales.each do |locale|
@@ -251,21 +254,29 @@ class Page < ActiveRecord::Base
           end
         end
       end
+
+      # Tags
       self.tags.to_xml(builder: xml, skip_instruct: true, only: [:name])
+
+      # Images
       if options[:images]
         xml.images do |images_xml|
-          self.page_images.each{|page| page.to_xml(builder: images_xml, skip_instruct: true, only: [:image_id, :primary])}
+          self.page_images.each{ |page_image| page_image.to_xml(builder: images_xml, skip_instruct: true, only: [:image_id, :primary]) }
         end
       end
+
+      # Comments
       if options[:comments]
         xml.comments do |comments_xml|
           self.comments.each{|comment| comment.to_xml(except: [:page_id], builder: comments_xml, skip_instruct: true)}
         end
       end
+
+      # Subpages
       if options[:pages]
         subpages = (options[:pages] == :all) ? self.children : self.pages
         xml.pages do |pages_xml|
-          self.pages.each{|page| page.to_xml(options.merge({builder: pages_xml, skip_instruct: true}))}
+          self.pages.each{ |page| page.to_xml(options.merge({builder: pages_xml, skip_instruct: true})) }
         end
       end
     end
