3440067e5ab247e50c71db4b3da4a2ec7f2d2ee0
Inge Jørgensen <inge@manualdesign.no>
2013-05-27 18:58:45 +0200

    Precompile admin assets

--- lib/pages_core/engine.rb
+++ lib/pages_core/engine.rb
@@ -2,5 +2,9 @@
 
 module PagesCore
   class Engine < Rails::Engine
+    # Enable asset precompilation
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w{pages/admin.js pages/admin.css pages/admin/print.css pages/errors.css}
+    end
   end
 end
