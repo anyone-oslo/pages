eb466c1c35c5af5921abd2ebb3d72018ffff7ddb
Inge Jørgensen <inge@manualdesign.no>
2013-06-03 15:23:17 +0200

    Deploy tweaks

--- deploy/pages.rb
+++ deploy/pages.rb
@@ -2,11 +2,6 @@
 
 namespace :pages do
 
-  desc "Fix migrations"
-  task :fix_migrations, :roles => [:db] do
-    run "cd #{release_path} && bundle exec rake pages:update:fix_migrations RAILS_ENV=production"
-  end
-
   desc "Create shared directories"
   task :create_shared_dirs, :roles => [:web,:app] do
     run "mkdir -p #{deploy_to}/#{shared_dir}/cache"
@@ -16,13 +11,6 @@ namespace :pages do
     run "mkdir -p #{deploy_to}/#{shared_dir}/index"
     run "mkdir -p #{deploy_to}/#{shared_dir}/sphinx"
     run "mkdir -p #{deploy_to}/#{shared_dir}/binary-objects"
-  end
-
-  desc "Fix permissions"
-  task :fix_permissions, :roles => [:web, :app] do
-    run "chmod -R a+x #{deploy_to}/#{current_dir}/script/*"
-    run "chmod a+x    #{deploy_to}/#{current_dir}/public/dispatch.*"
-    run "chmod a+rwx  #{deploy_to}/#{current_dir}/public"
   end
 
   desc "Create symlinks"
