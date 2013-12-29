9e6c28bc25ec6effde70131f428812a10cdb4ff7
Inge Jørgensen <inge@manualdesign.no>
2013-12-29 14:10:25 +0100

    DomainBasedCache concern

---
+++ app/controllers/concerns/pages_core/domain_based_cache.rb
@@ -0,0 +1,24 @@
+module PagesCore
+  module DomainBasedCache
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :set_domain_based_cache_path
+    end
+
+    module ClassMethods
+      def default_page_cache_directory
+        @@default_page_cache_directory ||= ActionController::Base.page_cache_directory
+      end
+    end
+
+    protected
+
+    def set_domain_based_cache_path
+      if PagesCore.config(:domain_based_cache)
+        path = File.join(ApplicationController.default_page_cache_directory, request.domain)
+        ActionController::Base.page_cache_directory = path
+      end
+    end
+  end
+end
--- app/controllers/pages_core/application_controller.rb
+++ app/controllers/pages_core/application_controller.rb
@@ -2,22 +2,15 @@
 
 class PagesCore::ApplicationController < ActionController::Base
   include PagesCore::Authentication
+  include PagesCore::DomainBasedCache
   include PagesCore::ExceptionHandler
   include PagesCore::ProcessTitler
   include PagesCore::OpenidHelper
 
-  before_action :domain_cache
   before_action :set_locale
   after_action  :set_content_language_header
 
   protected
-
-  def domain_cache
-    if PagesCore.config(:domain_based_cache)
-      @@default_page_cache_directory ||= @@page_cache_directory
-      @@page_cache_directory = File.join(@@default_page_cache_directory, request.domain)
-    end
-  end
 
   # Sets @locale from params[:locale], with Language.default as fallback
   def set_locale
