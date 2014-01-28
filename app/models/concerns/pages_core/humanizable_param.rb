fa0b3a4388df6675fa58ee2293c061105a80c786
Inge Jørgensen <inge@manualdesign.no>
2014-01-28 22:06:50 +0100

    Better URL slugging

--- app/models/concerns/pages_core/humanizable_param.rb
+++ app/models/concerns/pages_core/humanizable_param.rb
@@ -5,16 +5,10 @@ module PagesCore
     extend ActiveSupport::Concern
 
     def humanized_param(slug)
-      "#{id}-" + slug.downcase.gsub(/[^\w\s]/, '').split(/[^\w\d\-]+/).compact.join( "_" )
+      slug = slug.gsub(/[\[\{]/,'(')
+      slug = slug.gsub(/[\]\}]/,')')
+      slug = slug.gsub(/[^[[:alnum:]]()\-]+/,'-').gsub(/[\-]{2,}/,'-').gsub(/(^\-|\-$)/,'')
+      "#{self.id.to_s}-" + slug
     end
-
-    # This is the version from Sugar
-    # TODO: Switch to this
-    # def humanized_param(slug)
-    #   slug = slug.gsub(/[\[\{]/,'(')
-    #   slug = slug.gsub(/[\]\}]/,')')
-    #   slug = slug.gsub(/[^\w\d!$&'()*,;=\-]+/,'-').gsub(/[\-]{2,}/,'-').gsub(/(^\-|\-$)/,'')
-    #   "#{self.id.to_s};" + slug
-    # end
   end
 end
