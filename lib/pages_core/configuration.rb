840dd95114b1492cb207ecc469c632f99784035b
Inge Jørgensen <inge@manualdesign.no>
2014-02-05 15:12:32 +0100

    PagesCore::Configuration::SiteConfiguration replaced with PagesCore::Configuration::Pages

--- lib/pages_core/configuration.rb
+++ lib/pages_core/configuration.rb
@@ -5,7 +5,6 @@ load 'pages_core/configuration/pages.rb'
 
 load 'pages_core/configuration/handler.rb'
 load 'pages_core/configuration/proxy.rb'
-load 'pages_core/configuration/site_configuration.rb'
 
 module PagesCore
   module Configuration
