e717a03602519e7f1bf39d14f84ebe727ea32530
Inge Jørgensen <inge@manualdesign.no>
2014-03-28 17:19:57 +0100

    Param and locale in serialized page

--- app/serializers/page_serializer.rb
+++ app/serializers/page_serializer.rb
@@ -1,11 +1,15 @@
 class PageSerializer < ActiveModel::Serializer
-  attributes :id, :parent_page_id
+  attributes :id, :param, :parent_page_id, :locale
   attributes *PagesCore::Templates::TemplateConfiguration.all_blocks
   attributes :published_at, :pinned
 
   has_one  :image
   has_many :images
   has_many :pages
+
+  def param
+    object.to_param
+  end
 
   def image
     object.page_images.where(primary: true).try(:first)
