1fa8747704bfe33421daa7a9bf6555aa81ce7656
Inge Jørgensen <inge@manualdesign.no>
2014-09-22 13:49:34 +0200

    Converted PageFile to use Shrouded

--- app/models/page_file.rb
+++ app/models/page_file.rb
@@ -1,21 +1,14 @@
 # encoding: utf-8
 
 class PageFile < ActiveRecord::Base
-
-  FORMATS = {
-    'audio/mpeg'      => :mp3,
-    'image/gif'       => :gif,
-    'image/jpeg'      => :jpg,
-    'image/jpg'       => :jpg,
-    'image/pjpeg'     => :jpg,
-    'image/png'       => :png,
-    'application/pdf' => :pdf
-  }
+  include Shrouded::Model
 
   belongs_to :page
-  belongs_to :binary, dependent: :destroy
 
-  validates_presence_of :binary_id
+  validates_data_presence
+  validates :content_type, presence: true
+  validates :filename, presence: true
+  validates :content_length, presence: true
 
   acts_as_list scope: :page
 
@@ -23,31 +16,28 @@ class PageFile < ActiveRecord::Base
     attribute :description
   end
 
-  validate do |file|
-    file.name = File.basename(file.filename, ".*") unless file.name?
-    file.content_type = 'image/jpeg' if file.content_type == 'image/x-citrix-pjpeg' # Fuck you, Citrix
-    file.content_type = 'image/gif'  if file.content_type == 'image/x-citrix-gif'   # Fuck you, Citrix
-  end
+  before_validation :set_name_from_filename
 
-  def file=(file)
-    self.filename     = file.original_filename rescue File.basename( file.path )
-    self.filesize     = file.size
-    self.content_type = file.content_type
-    self.binary       = Binary.create unless self.binary
-    self.binary.data  = file.read
-    self.binary.save
-  end
-
-  def data
-    self.binary.data
+  class << self
+    def formats
+      {
+        'audio/mpeg'      => :mp3,
+        'image/gif'       => :gif,
+        'image/jpeg'      => :jpg,
+        'image/jpg'       => :jpg,
+        'image/pjpeg'     => :jpg,
+        'image/png'       => :png,
+        'application/pdf' => :pdf
+      }
+    end
   end
 
   def format?
-    (self.content_type && FORMATS.keys.include?(self.content_type)) ? true : false
+    (self.content_type && self.class.formats.keys.include?(self.content_type)) ? true : false
   end
 
   def format
-    FORMATS[self.content_type]
+    self.class.formats[self.content_type]
   end
 
   def filename_extension
@@ -70,4 +60,11 @@ class PageFile < ActiveRecord::Base
     end
   end
 
+  private
+
+  def set_name_from_filename
+    if self.filename? && !self.name?
+      self.name = File.basename(self.filename, ".*")
+    end
+  end
 end
