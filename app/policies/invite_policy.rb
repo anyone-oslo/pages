b03607f126f9d474331a4563baeb11af77fc3411
Inge Jørgensen <inge@manualdesign.no>
2014-10-10 16:06:37 +0200

    Invite policy

---
+++ app/policies/invite_policy.rb
@@ -0,0 +1,25 @@
+class InvitePolicy < Policy
+  def index?
+    true
+  end
+
+  def reorder?
+    user.has_role?(:users)
+  end
+
+  def new?
+    user.has_role?(:users)
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    user.has_role?(:users)
+  end
+
+  def accept?
+    true
+  end
+end
