eff203f1e8424ae02476dc3a338590bdb29c2a0b
Inge Jørgensen <inge@manualdesign.no>
2014-10-31 17:15:22 +0100

    Default to delayed_job queue adapter

--- lib/pages_core/engine.rb
+++ lib/pages_core/engine.rb
@@ -2,6 +2,10 @@
 
 module PagesCore
   class Engine < Rails::Engine
+    initializer :active_job do |config|
+      ActiveJob::Base.queue_adapter = :delayed_job
+    end
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
