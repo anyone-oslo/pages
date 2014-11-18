2d66f0d9c52a28ad88da1c8f4e26c487e0cf04ec
Inge Jørgensen <inge@manualdesign.no>
2014-11-18 10:36:43 +0100

    Use SASS for concatting stylesheets out of the box

--- lib/rails/generators/pages_core/frontend/frontend_generator.rb
+++ lib/rails/generators/pages_core/frontend/frontend_generator.rb
@@ -12,27 +12,42 @@ module PagesCore
       end
 
       def create_layout
-        copy_file 'layout.html.erb', File.join('app/views/layouts/application.html.erb')
+        copy_file('layout.html.erb',
+                  File.join('app/views/layouts/application.html.erb'))
       end
 
-      def create_application_css
-        template 'application.css.erb', File.join('app/assets/stylesheets/application.css')
+      def create_application_scss
+        template('application.css.scss.erb',
+                 File.join('app/assets/stylesheets/application.css.scss'))
+      end
+
+      def remove_application_css
+        remove_file('app/assets/stylesheets/application.css')
       end
 
       def create_normalize_css
-        template 'normalize.css.erb', File.join('app/assets/stylesheets/normalize.css')
+        template('normalize.css.erb',
+                 File.join('vendor/assets/stylesheets/normalize.css'))
       end
 
-      def create_mixins_css
-        template 'mixins.css.scss.erb', File.join('app/assets/stylesheets/mixins.css.scss')
+      def create_breakpoints_css
+        template('breakpoints.css.scss.erb',
+                 File.join('app/assets/stylesheets/mixins/breakpoints.css.scss'))
+      end
+
+      def create_hidpi_css
+        template('hidpi.css.scss.erb',
+                 File.join('app/assets/stylesheets/mixins/hidpi.css.scss'))
       end
 
       def create_base_css
-        template 'base.css.scss.erb', File.join('app/assets/stylesheets/components/base.css.scss')
+        template('base.css.scss.erb',
+                 File.join('app/assets/stylesheets/components/base.css.scss'))
       end
 
       def create_application_js
-        template 'application.js.erb', File.join('app/assets/javascripts/application.js')
+        template('application.js.erb',
+                 File.join('app/assets/javascripts/application.js'))
       end
     end
   end
