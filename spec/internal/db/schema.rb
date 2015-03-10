2c217eb3595f6f2f1a46fe85444cdf0ac6096970
Inge Jørgensen <inge@manualdesign.no>
2015-03-10 18:17:39 +0100

    Updates gems and schema.rb

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -11,79 +11,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007173000) do
+ActiveRecord::Schema.define(version: 20150204130800) do
 
-  create_table "binaries", force: true do |t|
-    t.string "sha1_hash"
+  create_table "binaries", force: :cascade do |t|
+    t.string "sha1_hash", limit: 255
   end
 
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.integer  "position"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "slug",       limit: 255
+    t.integer  "position",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "categories", ["slug"], name: "index_categories_on_slug", using: :btree
 
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0
-    t.integer  "attempts",   default: 0
-    t.text     "handler"
-    t.string   "last_error"
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   limit: 4,     default: 0
+    t.integer  "attempts",   limit: 4,     default: 0
+    t.text     "handler",    limit: 65535
+    t.text     "last_error", limit: 65535
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "queue"
+    t.string   "locked_by",  limit: 255
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "queue",      limit: 255
   end
 
-  create_table "images", force: true do |t|
-    t.string   "name"
-    t.string   "byline"
-    t.text     "description"
-    t.string   "filename",       null: false
-    t.string   "content_type",   null: false
+  create_table "images", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.string   "byline",         limit: 255
+    t.text     "description",    limit: 65535
+    t.string   "filename",       limit: 255,   null: false
+    t.string   "content_type",   limit: 255,   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "content_hash",   null: false
-    t.integer  "content_length", null: false
-    t.string   "colorspace",     null: false
-    t.integer  "real_width",     null: false
-    t.integer  "real_height",    null: false
-    t.integer  "crop_width"
-    t.integer  "crop_height"
-    t.integer  "crop_start_x"
-    t.integer  "crop_start_y"
-    t.integer  "crop_gravity_x"
-    t.integer  "crop_gravity_y"
+    t.string   "content_hash",   limit: 255,   null: false
+    t.integer  "content_length", limit: 4,     null: false
+    t.string   "colorspace",     limit: 255,   null: false
+    t.integer  "real_width",     limit: 4,     null: false
+    t.integer  "real_height",    limit: 4,     null: false
+    t.integer  "crop_width",     limit: 4
+    t.integer  "crop_height",    limit: 4
+    t.integer  "crop_start_x",   limit: 4
+    t.integer  "crop_start_y",   limit: 4
+    t.integer  "crop_gravity_x", limit: 4
+    t.integer  "crop_gravity_y", limit: 4
   end
 
-  create_table "invite_roles", force: true do |t|
-    t.integer  "invite_id"
-    t.string   "name"
+  create_table "invite_roles", force: :cascade do |t|
+    t.integer  "invite_id",  limit: 4
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "invites", force: true do |t|
-    t.integer  "user_id"
-    t.string   "email"
-    t.string   "token"
+  create_table "invites", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "email",      limit: 255
+    t.string   "token",      limit: 255
     t.datetime "sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "localizations", force: true do |t|
-    t.integer  "localizable_id"
-    t.string   "localizable_type"
-    t.string   "name"
-    t.string   "locale"
-    t.text     "value"
+  create_table "localizations", force: :cascade do |t|
+    t.integer  "localizable_id",   limit: 4
+    t.string   "localizable_type", limit: 255
+    t.string   "name",             limit: 255
+    t.string   "locale",           limit: 255
+    t.text     "value",            limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -91,61 +91,61 @@ ActiveRecord::Schema.define(version: 20141007173000) do
   add_index "localizations", ["localizable_id", "localizable_type", "name", "locale"], name: "by_foreign_key", unique: true, using: :btree
   add_index "localizations", ["localizable_id", "localizable_type"], name: "by_association", using: :btree
 
-  create_table "page_comments", force: true do |t|
-    t.integer  "page_id"
-    t.string   "remote_ip"
-    t.string   "name"
-    t.string   "email"
-    t.string   "url"
-    t.text     "body"
+  create_table "page_comments", force: :cascade do |t|
+    t.integer  "page_id",    limit: 4
+    t.string   "remote_ip",  limit: 255
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.string   "url",        limit: 255
+    t.text     "body",       limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "page_files", force: true do |t|
-    t.integer  "page_id"
-    t.integer  "position"
-    t.string   "name"
-    t.string   "filename",       null: false
-    t.string   "content_type",   null: false
-    t.integer  "content_length", null: false
+  create_table "page_files", force: :cascade do |t|
+    t.integer  "page_id",        limit: 4
+    t.integer  "position",       limit: 4
+    t.string   "name",           limit: 255
+    t.string   "filename",       limit: 255, null: false
+    t.string   "content_type",   limit: 255, null: false
+    t.integer  "content_length", limit: 4,   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "content_hash",   null: false
+    t.string   "content_hash",   limit: 255, null: false
   end
 
-  create_table "page_images", force: true do |t|
-    t.integer "page_id"
-    t.integer "image_id"
-    t.integer "position"
-    t.boolean "primary",  default: false, null: false
+  create_table "page_images", force: :cascade do |t|
+    t.integer "page_id",  limit: 4
+    t.integer "image_id", limit: 4
+    t.integer "position", limit: 4
+    t.boolean "primary",  limit: 1, default: false, null: false
   end
 
   add_index "page_images", ["page_id", "primary"], name: "index_page_images_on_page_id_and_primary", using: :btree
   add_index "page_images", ["page_id"], name: "index_page_images_on_page_id", using: :btree
 
-  create_table "pages", force: true do |t|
-    t.integer  "parent_page_id"
-    t.integer  "position"
-    t.string   "byline"
-    t.string   "template"
+  create_table "pages", force: :cascade do |t|
+    t.integer  "parent_page_id",   limit: 4
+    t.integer  "position",         limit: 4
+    t.string   "byline",           limit: 255
+    t.string   "template",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
-    t.integer  "status",           default: 0,     null: false
-    t.string   "content_order"
-    t.boolean  "feed_enabled",     default: false, null: false
+    t.integer  "user_id",          limit: 4
+    t.integer  "status",           limit: 4,   default: 0,     null: false
+    t.string   "content_order",    limit: 255
+    t.boolean  "feed_enabled",     limit: 1,   default: false, null: false
     t.datetime "published_at"
-    t.string   "redirect_to"
-    t.integer  "image_id"
-    t.boolean  "comments_allowed", default: true,  null: false
-    t.string   "image_link"
-    t.boolean  "news_page",        default: false, null: false
-    t.boolean  "autopublish",      default: false, null: false
-    t.string   "unique_name"
-    t.integer  "comments_count",   default: 0,     null: false
+    t.string   "redirect_to",      limit: 255
+    t.integer  "image_id",         limit: 4
+    t.boolean  "comments_allowed", limit: 1,   default: true,  null: false
+    t.string   "image_link",       limit: 255
+    t.boolean  "news_page",        limit: 1,   default: false, null: false
+    t.boolean  "autopublish",      limit: 1,   default: false, null: false
+    t.string   "unique_name",      limit: 255
+    t.integer  "comments_count",   limit: 4,   default: 0,     null: false
     t.datetime "last_comment_at"
-    t.boolean  "pinned",           default: false, null: false
+    t.boolean  "pinned",           limit: 1,   default: false, null: false
   end
 
   add_index "pages", ["parent_page_id"], name: "index_pages_on_parent_page_id", using: :btree
@@ -154,57 +154,66 @@ ActiveRecord::Schema.define(version: 20141007173000) do
   add_index "pages", ["status"], name: "index_pages_on_status", using: :btree
   add_index "pages", ["user_id"], name: "index_pages_on_user_id", using: :btree
 
-  create_table "pages_categories", id: false, force: true do |t|
-    t.integer "page_id"
-    t.integer "category_id"
+  create_table "pages_categories", id: false, force: :cascade do |t|
+    t.integer "page_id",     limit: 4
+    t.integer "category_id", limit: 4
   end
 
   add_index "pages_categories", ["category_id"], name: "index_pages_categories_on_category_id", using: :btree
   add_index "pages_categories", ["page_id"], name: "index_pages_categories_on_page_id", using: :btree
 
-  create_table "password_reset_tokens", force: true do |t|
-    t.integer  "user_id"
-    t.string   "token"
+  create_table "password_reset_tokens", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "token",      limit: 255
     t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "roles", force: true do |t|
-    t.integer  "user_id"
-    t.string   "name"
+  create_table "roles", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "roles", ["user_id"], name: "index_roles_on_user_id", using: :btree
 
-  create_table "taggings", force: true do |t|
-    t.integer "tag_id"
-    t.integer "taggable_id"
-    t.string  "taggable_type"
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255
+    t.text     "data",       limit: 65535
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id",        limit: 4
+    t.integer "taggable_id",   limit: 4
+    t.string  "taggable_type", limit: 255
   end
 
   add_index "taggings", ["tag_id"], name: "by_tag_id", using: :btree
   add_index "taggings", ["taggable_type", "taggable_id"], name: "by_taggable", using: :btree
 
-  create_table "tags", force: true do |t|
-    t.string "name"
+  create_table "tags", force: :cascade do |t|
+    t.string "name", limit: 255
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "hashed_password"
-    t.string   "name"
-    t.string   "email"
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        limit: 255
+    t.string   "hashed_password", limit: 255
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
     t.datetime "last_login_at"
-    t.integer  "created_by"
+    t.integer  "created_by",      limit: 4
     t.datetime "created_at"
-    t.text     "persistent_data"
-    t.boolean  "activated",       default: false, null: false
-    t.integer  "image_id"
+    t.text     "persistent_data", limit: 65535
+    t.boolean  "activated",       limit: 1,     default: false, null: false
+    t.integer  "image_id",        limit: 4
   end
 
 end
