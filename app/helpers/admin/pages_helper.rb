34eb77f97f4e96f204c6fa24ec5b4adbd2a1dd97
Inge Jørgensen <inge@manualdesign.no>
2016-02-10 15:00:49 +0100

    Improves PagesHelper

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -2,29 +2,33 @@
 
 module Admin
   module PagesHelper
-    def page_block_field(form, block_name, block_options)
+    def page_block_classes(class_name, block_options = {})
+      [class_name, block_options[:class]].join(" ")
+    end
+
+    def page_block_field_options(block_options = {})
+      opts = { placeholder: block_options[:placeholder] }
       if block_options[:size] == :field
-        labelled_field(
-          form.text_field(
-            block_name,
-            class:       ['text', block_options[:class]].join(" "),
-            placeholder: block_options[:placeholder]
-          ),
-          block_options[:title],
-          errors:      form.object.errors[block_name],
-          description: block_options[:description]
-        )
+        opts.merge(class: page_block_classes("rich", block_options))
       else
-        labelled_field form.text_area(
-          block_name,
-          rows:        (block_options[:size] == :large ? 15 : 5),
-          class:       ['rich', block_options[:class]].join(" "),
-          placeholder: block_options[:placeholder]
-        ),
-          block_options[:title],
-          errors:      form.object.errors[block_name],
-          description: block_options[:description]
+        opts.merge(
+          class: page_block_classes("rich", block_options),
+          rows: (block_options[:size] == :large ? 15 : 5)
+        )
       end
+    end
+
+    def page_block_field(form, block_name, block_options)
+      labelled_field(
+        form.send(
+          block_options[:size] == :field ? :text_field : :text_area,
+          block_name,
+          page_block_field_options(block_options)
+        ),
+        block_options[:title],
+        errors: form.object.errors[block_name],
+        description: block_options[:description]
+      )
     end
 
     def available_templates_for_select
@@ -54,9 +58,9 @@ module Admin
     end
 
     def publish_time(time)
-      if time.year != Time.now.year
+      if time.year != Time.zone.now.year
         time.strftime("on %b %d %Y at %H:%M")
-      elsif time.to_date != Time.now.to_date
+      elsif time.to_date != Time.zone.now.to_date
         time.strftime("on %b %Y at %H:%M")
       else
         time.strftime("at %H:%M")
