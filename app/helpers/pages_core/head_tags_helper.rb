a78fcca5834a55d7ea8afc0f423d669d26bde129
Inge Jørgensen <inge@manualdesign.no>
2016-03-30 15:19:39 +0200

    Updated Google Analytics tracking code

--- app/helpers/pages_core/head_tags_helper.rb
+++ app/helpers/pages_core/head_tags_helper.rb
@@ -55,17 +55,12 @@ module PagesCore
     #
     def google_analytics_tags(account_id)
       javascript_tag(
-        "var _gaq = _gaq || [];\n" \
-          "_gaq.push(['_setAccount', '#{account_id}']);\n" \
-          "_gaq.push(['_trackPageview']);\n" \
-          "(function() {\n" \
-          "var ga = document.createElement('script'); " \
-          "ga.type = 'text/javascript'; ga.async = true;\n" \
-          "ga.src = ('https:' == document.location.protocol ? 'https://ssl' " \
-          ": 'http://www') + '.google-analytics.com/ga.js';\n" \
-          "var s = document.getElementsByTagName('script')[0]; " \
-          "s.parentNode.insertBefore(ga, s);\n" \
-          "})();"
+        "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||" \
+          "function(){\n (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new " \
+          "Date();a=s.createElement(o),\nm=s.getElementsByTagName(o)[0];" \
+          "a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n})(window," \
+          "document,'script','//www.google-analytics.com/analytics.js','ga');" \
+          "\n\nga('create', '#{account_id}', 'auto');\nga('send', 'pageview');"
       )
     end
 
