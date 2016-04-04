f4ab7cc15a9d3e6e6b5a380603ff144149b0e627
Inge Jørgensen <inge@manualdesign.no>
2016-04-04 15:13:00 +0200

    Refactored RichTextArea to a class

--- app/assets/javascripts/pages/admin/features/rich_text.es6.jsx
+++ app/assets/javascripts/pages/admin/features/rich_text.es6.jsx
@@ -1,141 +1,124 @@
-(function () {
-  class TextileDecorator {
-    blockquote(str) { return ["bq. ", str, ""]; }
-    bold(str) { return ["<b>", str, "</b>"]; }
-    emphasis(str) { return ["<i>", str, "</i>"]; }
-    h1(str) { return ["h1. ", str, ""]; }
-    h2(str) { return ["h2. ", str, ""]; }
-    h3(str) { return ["h3. ", str, ""]; }
-    h4(str) { return ["h4. ", str, ""]; }
-    h5(str) { return ["h5. ", str, ""]; }
-    h6(str) { return ["h6. ", str, ""]; }
-    link(url, name) { return ["\"", name, `\":${url}`]; }
-    email(address, name) { return ["\"", name, `\":mailto:${address}`]; }
+class TextileDecorator {
+  blockquote(str) { return ["bq. ", str, ""]; }
+  bold(str) { return ["<b>", str, "</b>"]; }
+  emphasis(str) { return ["<i>", str, "</i>"]; }
+  h1(str) { return ["h1. ", str, ""]; }
+  h2(str) { return ["h2. ", str, ""]; }
+  h3(str) { return ["h3. ", str, ""]; }
+  h4(str) { return ["h4. ", str, ""]; }
+  h5(str) { return ["h5. ", str, ""]; }
+  h6(str) { return ["h6. ", str, ""]; }
+  link(url, name) { return ["\"", name, `\":${url}`]; }
+  email(address, name) { return ["\"", name, `\":mailto:${address}`]; }
 
-    list(str) {
-      return ["", ((() => {
-        let iterable = str.split("\n");
-        var result = [];
-        for (var i = 0, line; i < iterable.length; i++) {
-          line = iterable[i];
-          result.push("* " + line);
-        }
-        return result;
-      })()).join("\n"), ""];
-    }
-
-    orderedList(str) {
-      return ["", ((() => {
-        let iterable = str.split("\n");
-        var result = [];
-        for (var i = 0, line; i < iterable.length; i++) {
-          line = iterable[i];
-          result.push("# " + line);
-        }
-        return result;
-      })()).join("\n"), ""];
-    }
+  list(str) {
+    return ["", str.split("\n").map(l => "* " + l).join("\n"), ""];
   }
 
-  // Gets the selected text from an element
-  let getSelection = elem => $(elem).getSelection().text
+  orderedList(str) {
+    return ["", str.split("\n").map(l => "# " + l).join("\n"), ""];
+  }
+}
 
-  // Gets the selected range from an element
-  let getSelectionRange = function(elem) {
-    if (typeof elem.selectionStart !== "undefined") {
-      return [elem.selectionStart, elem.selectionEnd];
+class RichTextArea {
+  constructor(textarea) {
+    this.textarea = textarea;
+    this.decorator = new TextileDecorator();
+    this.toolbar = $("<ul class=\"rich-text-toolbar\"></ul>")
+                      .insertBefore(this.textarea);
+    this.addButtons();
+  }
+
+  adjustSelection(callback) {
+    let length = this.getSelection().length;
+    let [start, end] = this.getSelectionRange();
+    let [replacementLength, prefixLength] = callback();
+    let newEnd = (end + (replacementLength - length) + prefixLength);
+    let newStart = start === end ? newEnd : (start + prefixLength);
+    return this.setSelectionRange(newStart, newEnd);
+  };
+
+  getSelection() {
+    return $(this.textarea).getSelection().text;
+  }
+
+  getSelectionRange() {
+    if (typeof this.textarea.selectionStart !== "undefined") {
+      return [this.textarea.selectionStart, this.textarea.selectionEnd];
     } else {
       return [0, 0];
     }
   };
 
-  // Sets a new selection range for object
-  let setSelectionRange = function(elem, start, end) {
-    if (typeof elem.setSelectionRange !== "undefined") {
-      return elem.setSelectionRange(start, end);
-    }
-  };
-
-  let adjustSelection = function(elem, callback) {
-    let length = getSelection(elem).length;
-    let [start, end] = getSelectionRange(elem);
-    let [replacementLength, prefixLength] = callback();
-    let newEnd = (end + (replacementLength - length) + prefixLength);
-    let newStart = start === end ? newEnd : (start + prefixLength);
-    return setSelectionRange(elem, newStart, newEnd);
-  };
-
-  let replaceSelection = function(elem, prefix, replacement, postfix) {
-    return adjustSelection(elem, function() {
-      $(elem).replaceSelection(prefix + replacement + postfix);
-      $(elem).focus();
+  replaceSelection(prefix, replacement, postfix) {
+    return this.adjustSelection(() => {
+      $(this.textarea).replaceSelection(prefix + replacement + postfix);
+      $(this.textarea).focus();
       return [replacement.length, prefix.length];
     });
   };
 
-  let RichTextArea = function(textarea) {
-    // Only apply it once
-    if (textarea.richtext) { return this; }
-    textarea.richtext = true;
+  // Sets a new selection range for object
+  setSelectionRange(start, end) {
+    if (typeof this.textarea.setSelectionRange !== "undefined") {
+      return this.textarea.setSelectionRange(start, end);
+    }
+  };
 
-    let toolbar = $("<ul class=\"rich-text-toolbar\"></ul>")
-                    .insertBefore(textarea);
+  addButton(name, className, callback) {
+    var link = $(
+      `<a title=\"${name}\" class=\"${className}\">` +
+      `<i class=\"fa fa-${className}\"></i></a>`
+    );
 
-    let decorator = () => new TextileDecorator();
+    link.click(() => {
+      let [prefix, replacement, postfix] = callback(this.getSelection());
+      this.replaceSelection(prefix, replacement, postfix);
+    });
 
-    var addButton = function(name, className, callback) {
-      var link = $(
-        `<a title=\"${name}\" class=\"${className}\">` +
-        `<i class=\"fa fa-${className}\"></i></a>`
-      );
+    $("<li class=\"button\"></li>").append(link).appendTo(this.toolbar);
+  };
 
-      link.click(function() {
-        let [prefix, replacement, postfix] = callback(getSelection(textarea));
-        return replaceSelection(textarea, prefix, replacement, postfix);
-      });
-
-      return $("<li class=\"button\"></li>").append(link).appendTo(toolbar);
-    };
-
+  addButtons() {
     // Bold button
-    addButton("Bold", "bold", s => decorator().bold(s));
+    this.addButton("Bold", "bold", s => this.decorator.bold(s));
 
     // Italic button
-    addButton("Italics", "italic", s => decorator().emphasis(s));
+    this.addButton("Italics", "italic", s => this.decorator.emphasis(s));
 
     // Heading buttons
-    addButton("Heading 2", "header h2", s => decorator().h2(s));
-    addButton("Heading 3", "header h3", s => decorator().h3(s));
-    addButton("Heading 4", "header h4", s => decorator().h4(s));
+    this.addButton("Heading 2", "header h2", s => this.decorator.h2(s));
+    this.addButton("Heading 3", "header h3", s => this.decorator.h3(s));
+    this.addButton("Heading 4", "header h4", s => this.decorator.h4(s));
 
     // Block Quote
-    addButton("Block Quote", "quote-left", s => decorator().blockquote(s))
+    this.addButton("Block Quote", "quote-left", s => this.decorator.blockquote(s))
 
     // List
-    addButton("List", "list-ul", s => decorator().list(s));
+    this.addButton("List", "list-ul", s => this.decorator.list(s));
 
     // Ordered list
-    addButton("Ordered list", "list-ol", s => decorator().orderedList(s));
+    this.addButton("Ordered list", "list-ol", s => this.decorator.orderedList(s));
 
     // Link button
-    addButton("Link", "link", function(selection) {
+    this.addButton("Link", "link", selection => {
       let name = selection.length > 0 ? selection : "Link text";
       var url = prompt("Enter link URL", "");
       url = url.length > 0 ? url : "http://example.com/";
       url = url.replace(/^(?!(f|ht)tps?:\/\/)/, 'http://');
-      return decorator().link(url, name);
+      return this.decorator.link(url, name);
     });
 
     // Email button
-    return addButton("Email link", "envelope", function(selection) {
+    this.addButton("Email link", "envelope", selection => {
       let name = selection.length > 0 ? selection : "Link text";
       var address = prompt("Enter email address", "");
       address = address.length > 0 ? address : "example@example.com";
-      return decorator().email(address, name);
+      return this.decorator.email(address, name);
     });
-  };
+  }
+}
 
-  $(function() {
-    $('textarea.rich').each(function() { return new RichTextArea(this); });
-  });
-})();
+$(function() {
+  $('textarea.rich').each(function() { new RichTextArea(this); });
+});
