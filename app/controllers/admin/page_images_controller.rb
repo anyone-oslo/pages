400ca956280bccf1996d6c7045699aa754661ef6
Inge Jørgensen <inge@manualdesign.no>
2017-02-24 14:20:17 +0100

    Fix for image sorting

--- app/controllers/admin/page_images_controller.rb
+++ app/controllers/admin/page_images_controller.rb
@@ -23,7 +23,7 @@ module Admin
     def reorder
       @page_images = params[:ids].map { |id| PageImage.find(id) }
       @page_images.each_with_index do |pi, i|
-        pi.update(position: i)
+        pi.update(position: i + 1)
       end
       respond_to do |format|
         format.json do
