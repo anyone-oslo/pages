2b4acbba75d00191a11ddeaff6cb59fab1c7586c
Inge Jørgensen <inge@manualdesign.no>
2017-02-27 16:25:18 +0100

    Version 3.5.1

--- lib/pages_core/version.rb
+++ lib/pages_core/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module PagesCore
-  VERSION = "3.5.0".freeze unless PagesCore.const_defined?("VERSION")
+  VERSION = "3.5.1".freeze unless PagesCore.const_defined?("VERSION")
 end
