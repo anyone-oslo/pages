fc7aa8d729b319f1092044b0eefdf896d28ec5c5
Inge Jørgensen <inge@manualdesign.no>
2018-06-25 13:51:02 +0200

    Use stale checking for page files

--- app/controllers/pages_core/frontend/page_files_controller.rb
+++ app/controllers/pages_core/frontend/page_files_controller.rb
@@ -4,31 +4,15 @@ module PagesCore
       before_action :find_page_file, only: %i[show]
 
       def show
-        unless modified?(@page_file)
-          render(text: "304 Not Modified", status: 304) && return
+        if stale?(etag: @page_file, last_modified: @page_file.updated_at)
+          send_data(@page_file.data,
+                    filename: @page_file.filename,
+                    type: @page_file.content_type,
+                    disposition: "attachment")
         end
-
-        if @page_file.updated_at?
-          response.headers["Last-Modified"] = @page_file.updated_at.httpdate
-        end
-
-        send_data(@page_file.data,
-                  filename: @page_file.filename,
-                  type: @page_file.content_type,
-                  disposition: "attachment")
       end
 
       private
-
-      def modified?(page_file)
-        return true unless if_modified_since && page_file.updated_at?
-        page_file.updated_at > if_modified_since
-      end
-
-      def if_modified_since
-        return nil if request.env["HTTP_IF_MODIFIED_SINCE"].blank?
-        Time.rfc2822(request.env["HTTP_IF_MODIFIED_SINCE"])
-      end
 
       def find_page_file
         @page_file = PageFile.find(params[:id])
