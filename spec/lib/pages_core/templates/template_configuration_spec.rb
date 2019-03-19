7474f2b566175605900820ae1760867840cfc02f
Inge Jørgensen <inge@manualdesign.no>
2019-03-19 18:47:35 +0100

    Specs for TemplateConfiguration#all_blocks

---
+++ spec/lib/pages_core/templates/template_configuration_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe PagesCore::Templates::TemplateConfiguration do
+  describe "#all_blocks" do
+    subject { described_class.all_blocks }
+
+    let(:blocks) do
+      %i[name body headline excerpt boxout meta_title
+         meta_description open_graph_title open_graph_description]
+    end
+
+    it { is_expected.to match(blocks) }
+  end
+end
