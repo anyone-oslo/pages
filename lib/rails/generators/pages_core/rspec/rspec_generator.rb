9fe3106d725de990965754d846c5e1877aab9610
Inge Jørgensen <inge@anyone.no>
2020-06-09 02:06:15 +0200

    Remove CircleCI

--- lib/rails/generators/pages_core/rspec/rspec_generator.rb
+++ lib/rails/generators/pages_core/rspec/rspec_generator.rb
@@ -20,12 +20,6 @@ module PagesCore
         end
       end
 
-      def setup_circleci
-        @test_db =
-          Rails.configuration.database_configuration["test"]["database"]
-        template "circleci_config.yml", File.join(".circleci/config.yml")
-      end
-
       def setup_rspec
         create_file File.join(".rspec"), "--format Fuubar\n--colour\n" \
                                          "--require spec_helper"
