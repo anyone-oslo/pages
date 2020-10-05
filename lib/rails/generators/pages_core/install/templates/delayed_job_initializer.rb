d088128f7f9fe1c5696e7787aa32f2bbfe4c470f
Inge Jørgensen <inge@anyone.no>
2020-10-05 15:54:37 +0200

    DelayedJob should ignore Postmark::InactiveRecipientError

--- lib/rails/generators/pages_core/install/templates/delayed_job_initializer.rb
+++ lib/rails/generators/pages_core/install/templates/delayed_job_initializer.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
 Delayed::Worker.backend = :active_record
+
+if Object.const_defined?("Postmark")
+  class InvalidRecipientsPlugin < Delayed::Plugin
+    callbacks do |lifecycle|
+      lifecycle.around(:invoke_job) do |job, *args, &block|
+        # Forward the call to the next callback in the callback chain
+        block.call(job, *args)
+      rescue Postmark::InactiveRecipientError => e
+        Rails.logger.error "#{e.class}: #{e.message}"
+      end
+    end
+  end
+
+  Delayed::Worker.plugins << InvalidRecipientsPlugin
+end
