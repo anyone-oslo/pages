108769a7ab1c49c7c9813f8877f59ad26a4c08d0
Inge Jørgensen <inge@elektronaut.no>
2021-09-29 12:47:13 +0200

    Export React components used by custom modules

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -5,7 +5,10 @@ Rails.start();
 
 require("react_ujs");
 
+import Attachments from "./components/Attachments";
+import DateRangeSelect from "./components/DateRangeSelect";
 import EditableImage from "./components/EditableImage";
+import ImageGrid from "./components/ImageGrid";
 import ImageUploader from "./components/ImageUploader";
 import Modal from "./components/Modal";
 import PageDates from "./components/PageDates";
@@ -16,7 +19,10 @@ import RichTextArea from "./components/RichTextArea";
 import TagEditor from "./components/TagEditor";
 import Toast from "./components/Toast";
 
+window["Attachments"] = Attachments;
+window["DateRangeSelect"] = DateRangeSelect;
 window["EditableImage"] = EditableImage;
+window["ImageGrid"] = ImageGrid;
 window["ImageUploader"] = ImageUploader;
 window["Modal"] = Modal;
 window["PageDates"] = PageDates;
