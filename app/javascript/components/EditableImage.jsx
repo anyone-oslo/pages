a16d2415da2a985c073b60abb37e1a79003de755
Inge Jørgensen <inge@elektronaut.no>
2021-11-11 13:37:39 +0100

    Converted EditableImage and ImageUploader to function components

--- app/javascript/components/EditableImage.jsx
+++ app/javascript/components/EditableImage.jsx
@@ -1,65 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import ImageEditor from "./ImageEditor";
 import ModalStore from "./ModalStore";
 
-export default class EditableImage extends React.Component {
-  constructor(props) {
-    let image = props.image;
-    super(props);
-    this.state = {
-      image:  image,
-      src:    props.src,
-      width:  image.crop_width || image.real_width,
-      height: image.crop_height || image.real_height
-    };
-    this.openEditor = this.openEditor.bind(this);
-    this.update = this.update.bind(this);
-  }
+export default function EditableImage(props) {
+  const [image, setImage] = useState(props.image);
+  const [src, setSrc] = useState(props.src);
 
-  openEditor() {
+  const height = () => {
+    const width = image.crop_width || image.real_width;
+    const height = image.crop_height || image.real_height;
+    return Math.round((height / width) * props.width);
+  };
+
+  const updateImage = (updatedImage, src) => {
+    let newImage = { ...image, ...updatedImage };
+    setSrc(src);
+    setImage(newImage);
+    if (props.onUpdate) {
+      props.onUpdate(newImage, src);
+    }
+  };
+
+  const handleClick = (evt) => {
+    evt.preventDefault();
     ModalStore.dispatch({
       type: "OPEN",
-      payload: <ImageEditor image={this.state.image}
-                            caption={this.props.caption}
-                            locale={this.props.locale}
-                            locales={this.props.locales}
-                            onUpdate={this.update} />
+      payload: <ImageEditor image={image}
+                            caption={props.caption}
+                            locale={props.locale}
+                            locales={props.locales}
+                            onUpdate={updateImage} />
     });
-  }
+  };
 
-  height() {
-    let image = this.state.image;
-    let width = image.crop_width || image.real_width;
-    let height = image.crop_height || image.real_height;
-    return Math.round((height / width) * this.props.width);
-  }
+  const altWarning = !image.alternative[props.locale];
 
-  update(image, croppedImage) {
-    let newImage = { ...this.state.image, ...image };
-    this.setState({image: newImage,
-                   src: croppedImage});
-    if (this.props.onUpdate) {
-      this.props.onUpdate(newImage, croppedImage);
-    }
-  }
-
-  render() {
-    let altWarning = !this.state.image.alternative[this.props.locale];
-
-    return (
-      <div className="editable-image">
-        {altWarning &&
-         <span className="alt-warning" title="Alternative text is missing">
-           <i className="fa fa-exclamation-triangle icon" />
-         </span>}
-        <img src={this.state.src}
-             width={this.props.width}
-             height={this.height()}
-             onClick={this.openEditor} />
-      </div>
-    );
-  }
+  return (
+    <div className="editable-image">
+      {altWarning &&
+       <span className="alt-warning" title="Alternative text is missing">
+         <i className="fa fa-exclamation-triangle icon" />
+       </span>}
+      <img src={src}
+           width={props.width}
+           height={height()}
+           onClick={handleClick} />
+    </div>
+  );
 }
 
 EditableImage.propTypes = {
--- app/javascript/components/ImageUploader.jsx
+++ app/javascript/components/ImageUploader.jsx
@@ -1,144 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import EditableImage from "./EditableImage";
 import FileUploadButton from "./FileUploadButton";
 import ToastStore from "./ToastStore";
 import { post } from "../lib/request";
 
-export default class ImageUploader extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { dragover: false,
-                   uploading: false,
-                   image: props.image,
-                   src: props.src };
-    this.receiveFiles = this.receiveFiles.bind(this);
-    this.removeImage = this.removeImage.bind(this);
-    this.dragOver = this.dragOver.bind(this);
-    this.dragLeave = this.dragLeave.bind(this);
-    this.dragEnd = this.dragEnd.bind(this);
-    this.drop = this.drop.bind(this);
-  }
-
-  receiveFiles(files) {
-    if (files.length > 0) {
-      this.uploadImage(files[0]);
+function getFiles(dt) {
+  var files = [];
+  if (dt.items) {
+    for (let i = 0; i < dt.items.length; i++) {
+      if (dt.items[i].kind == "file") {
+        files.push(dt.items[i].getAsFile());
+      }
+    }
+  } else {
+    for (let i = 0; i < dt.files.length; i++) {
+      files.push(dt.files[i]);
     }
   }
+  return files;
+}
 
-  clearItems(dt) {
-    if (dt.items) {
-      for (var i = 0; i < dt.items.length; i++) {
-        dt.items.remove(i);
+export default function ImageUploader(props) {
+  const [uploading, setUploading] = useState(false);
+  const [dragover, setDragover] = useState(false);
+  const [image, setImage] = useState(props.image);
+  const [src, setSrc] = useState(props.src);
+
+  const handleDragOver = (evt) => {
+    evt.preventDefault();
+    setDragover(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragover(false);
+  };
+
+  const handleDragEnd = (evt) => {
+    if (evt.dataTransfer.items) {
+      for (var i = 0; i < evt.dataTransfer.items.length; i++) {
+        evt.dataTransfer.items.remove(i);
       }
     } else {
-      dt.clearData();
+      evt.dataTransfer.clearData();
     }
-  }
+    setDragover(false);
+  };
 
-  dragOver(evt) {
-    evt.preventDefault();
-    this.setState({ dragover: true });
-  }
-
-  dragLeave() {
-    this.setState({ dragover: false });
-  }
-
-  dragEnd(evt) {
-    this.clearItems(evt.dataTransfer);
-    this.setState({ dragover: false });
-  }
-
-  drop(evt) {
-    let files = this.getFiles(evt.dataTransfer);
+  const handleDrop = (evt) => {
+    let files = getFiles(evt.dataTransfer);
     evt.preventDefault();
     if (files.length > 0) {
-      this.uploadImage(files[0]);
+      uploadImage(files[0]);
     }
-  }
+  };
 
-  editableImage() {
-    let image = this.state.image;
-    if (!image) { return; }
-    return (
-      <div className="image">
-        <EditableImage image={image}
-                       src={this.state.src}
-                       width={this.props.width}
-                       caption={this.props.caption}
-                       locale={this.props.locale}
-                       locales={this.props.locales} />
-      </div>
-    );
-  }
-
-  getFiles(dt) {
-    var files = [];
-    if (dt.items) {
-      for (let i = 0; i < dt.items.length; i++) {
-        if (dt.items[i].kind == "file") {
-          files.push(dt.items[i].getAsFile());
-        }
-      }
-    } else {
-      for (let i = 0; i < dt.files.length; i++) {
-        files.push(dt.files[i]);
-      }
-    }
-    return files;
-  }
-
-  render() {
-    let image = this.state.image;
-    let id = image ? image.id : "";
-    let classes = ["image-uploader"];
-    if (this.state.uploading) {
-      classes.push("uploading");
-    } else if (this.state.dragover) {
-      classes.push("dragover");
-    }
-    return (
-      <div className={classes.join(" ")}
-           onDragOver={this.dragOver}
-           onDragLeave={this.dragLeave}
-           onDragEnd={this.dragEnd}
-           onDrop={this.drop}>
-        <input type="hidden"
-               name={this.props.attr}
-               value={id} />
-        {this.editableImage()}
-        <div className="ui-wrapper">
-          {this.state.uploading && (
-            <div className="ui">
-              Uploading image...
-            </div>
-          )}
-          {!this.state.uploading && (
-            <div className="ui">
-              <FileUploadButton type="image"
-                                multiline={true}
-                                callback={this.receiveFiles} />
-              {image && (
-                <a className="delete remove-image"
-                   href="#"
-                   onClick={this.removeImage}>
-                  Remove image
-                </a>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
-
-  removeImage(evt) {
+  const handleRemove = (evt) => {
     evt.preventDefault();
-    this.setState({image: null, src: null});
-  }
+    setImage(null);
+    setSrc(null);
+  };
 
-  uploadImage(file) {
+  const receiveFiles = (files) => {
+    if (files.length > 0) {
+      uploadImage(files[0]);
+    }
+  };
+
+  const uploadImage = (file) => {
     let validTypes = ["image/gif",
                       "image/jpeg",
                       "image/pjpeg",
@@ -151,32 +80,83 @@ export default class ImageUploader extends React.Component {
       return;
     }
 
-    let locale = this.props.locale;
-    let locales = this.props.locales ? Object.keys(this.props.locales) : [locale];
+    let locale = props.locale;
+    let locales = props.locales ? Object.keys(props.locales) : [locale];
 
-    let component = this;
     let data = new FormData();
-    this.setState({ image: null, src: null, dragover: false, uploading: true });
+
+    setImage(null);
+    setSrc(null);
+    setDragover(false);
+    setUploading(true);
+
     data.append("image[file]", file);
     locales.forEach((l) => {
-      data.append(`image[alternative][${l}]`, (this.props.alternative || ""));
+      data.append(`image[alternative][${l}]`, (props.alternative || ""));
     });
 
     post("/admin/images.json", data)
       .then(response => {
+        setUploading(false);
         if (response.status === "error") {
           ToastStore.dispatch({
             type: "ERROR",
             message: "Error uploading image: " + response.error
           });
-          component.setState({ uploading: false });
         } else {
-          component.setState({ uploading: false,
-                               image: response,
-                               src: response.thumbnail_url });
+          setSrc(response.thumbnail_url);
+          setImage(response);
         }
       });
+  };
+
+  let classes = ["image-uploader"];
+  if (uploading) {
+    classes.push("uploading");
+  } else if (dragover) {
+    classes.push("dragover");
   }
+  return (
+    <div className={classes.join(" ")}
+         onDragOver={handleDragOver}
+         onDragLeave={handleDragLeave}
+         onDragEnd={handleDragEnd}
+         onDrop={handleDrop}>
+      <input type="hidden"
+             name={props.attr}
+             value={image ? image.id : ""} />
+      {image &&
+       <div className="image">
+         <EditableImage image={image}
+                        src={src}
+                        width={props.width}
+                        caption={props.caption}
+                        locale={props.locale}
+                        locales={props.locales} />
+       </div>}
+      <div className="ui-wrapper">
+        {uploading && (
+          <div className="ui">
+            Uploading image...
+          </div>
+        )}
+        {!uploading && (
+          <div className="ui">
+            <FileUploadButton type="image"
+                              multiline={true}
+                              callback={receiveFiles} />
+            {image && (
+              <a className="delete remove-image"
+                 href="#"
+                 onClick={handleRemove}>
+                Remove image
+              </a>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 ImageUploader.propTypes = {
