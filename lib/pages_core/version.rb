634288f5578fd914eb3864b3db32b6d46a8afaad
Inge Jørgensen <inge@elektronaut.no>
2021-11-17 13:59:12 +0100

    Version 3.8.1

--- lib/pages_core/version.rb
+++ lib/pages_core/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module PagesCore
-  VERSION = "3.8.0" unless PagesCore.const_defined?("VERSION")
+  VERSION = "3.8.1" unless PagesCore.const_defined?("VERSION")
 end
