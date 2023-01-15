275d782988e387c07cbdc765e17c425328b68d3c
Inge Jørgensen <inge@elektronaut.no>
2023-01-15 18:25:03 +0100

    Inline error styles

--- lib/pages_core/engine.rb
+++ lib/pages_core/engine.rb
@@ -29,7 +29,6 @@ module PagesCore
       Rails.application.config.assets.precompile += %w[
         pages_core/admin-dist.js
         pages_core/admin.css
-        pages_core/errors.css
         pages_core/fonts/*.ttf
         pages_core/fonts/*.woff2
         pages/favicon.gif
