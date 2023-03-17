e455c4378790bfd6ef06bcb47fa16b0aa4110a9c
Inge Jørgensen <inge@elektronaut.no>
2023-03-17 15:05:28 +0100

    Use PostCSS for frontend framework

--- lib/rails/generators/pages_core/frontend/frontend_generator.rb
+++ lib/rails/generators/pages_core/frontend/frontend_generator.rb
@@ -14,17 +14,15 @@ module PagesCore
       end
 
       def create_css_framework
-        ["application.sass.scss",
-         "config.scss",
-         "components/base.scss",
-         "framework/breakpoints.scss",
-         "framework/clearfix.scss",
-         "framework/grid.scss",
-         "framework/grid_overlay.scss",
-         "framework/fonts.scss",
-         "global/colors.scss",
-         "global/typography.scss",
-         "vendor/normalize.css"].each do |f|
+        ["application.postcss.css",
+         "config.css",
+         "components/base.css",
+         "components/layout.css",
+         "global/animation.css",
+         "global/colors.css",
+         "global/fonts.css",
+         "global/grid.css",
+         "global/typography.css"].each do |f|
           template("stylesheets/#{f}", File.join("app/assets/stylesheets/#{f}"))
         end
       end
@@ -46,6 +44,24 @@ module PagesCore
             GridOverlay.start();
           JS
         end
+      end
+
+      def install_js_dependencies
+        run("yarn add autoprefixer cssnano normalize.css postcss " \
+            "postcss-calc postcss-cli postcss-import " \
+            "postcss-import-ext-glob postcss-mixins postcss-preset-env " \
+            "postcss-url")
+      end
+
+      def configure_postcss
+        copy_file("postcss.config.js",
+                  File.join("postcss.config.js"))
+      end
+
+      def configure_build_script
+        script = "postcss ./app/assets/stylesheets/application.postcss.css " \
+                 "-o ./app/assets/builds/application.css"
+        run %(npm set-script build:css "#{script}")
       end
 
       def remove_application_css
