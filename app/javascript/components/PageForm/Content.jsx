fdf9d64a2b33a8e7136991f41dc864693cc8422d
Inge Jørgensen <inge@elektronaut.no>
2023-03-26 16:44:40 +0200

    All tabs Reactified

--- app/javascript/components/PageForm/Content.jsx
+++ app/javascript/components/PageForm/Content.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import usePage, { blockValue, errorsOn, updateBlock } from "./usePage";
+import usePage, { blockValue, errorsOn } from "./usePage";
+import LabelledField from "../LabelledField";
+import TagEditor from "../TagEditor";
 import Block from "./Block";
 import Dates from "./Dates";
 
 export default function Content(props) {
-  const { locale, locales, templates } = props;
-  const [page, setPage, templateConfig] = usePage(props.page, templates);
+  const [state, dispatch] = usePage({
+    locales: props.locales,
+    locale: props.locale,
+    page: props.page,
+    templates: props.templates
+  });
 
-  const inputDir = (locales && locales[locale] && locales[locale].dir) || "ltr";
+  const { page, locale, inputDir, templateConfig } = state;
+
+  const handleChange = (attr) => (value) => {
+    dispatch({ type: "update", payload: { [attr]: value } });
+  };
 
   return (
     <React.Fragment>
@@ -19,12 +29,18 @@ export default function Content(props) {
                errors={errorsOn(page, b.name)}
                dir={inputDir}
                lang={locale}
-               onChange={updateBlock(page, setPage, b, locale)}
-               value={blockValue(page, b, locale)} />)}
+               onChange={handleChange(b.name)}
+               value={blockValue(state, b)} />)}
       {templateConfig.dates &&
        <Dates starts_at={page.starts_at}
               ends_at={page.ends_at}
               all_day={page.all_day} />}
+      {templateConfig.tags &&
+       <LabelledField label="Tags">
+         <TagEditor name="page[serialized_tags]"
+                    enabled={page.enabled_tags}
+                    tags={page.tags_and_suggestions} />
+       </LabelledField>}
     </React.Fragment>
   );
 }
--- app/resources/admin/page_resource.rb
+++ app/resources/admin/page_resource.rb
@@ -3,6 +3,9 @@
 module Admin
   class PageResource
     include Alba::Resource
+    include Rails.application.routes.url_helpers
+    include PagesCore::PagePathHelper
+    include DynamicImage::Helper
 
     attributes :id, :starts_at, :ends_at, :all_day, :status, :published_at,
                :pinned, :template, :unique_name, :feed_enabled, :news_page,
@@ -11,7 +14,7 @@ module Admin
     PagesCore::Templates::TemplateConfiguration.all_blocks.each do |attr|
       attribute attr do
         if object.template_config.block(attr)[:localized]
-          localized_attribute(attr)
+          localized_attribute(object, attr)
         else
           object.send(attr)
         end
@@ -25,11 +28,58 @@ module Admin
       end
     end
 
+    attribute :urls do
+      if object.id?
+        localized_objects.filter(&:name?).each_with_object({}) do |p, obj|
+          obj[p.locale] = page_path(p.locale, p)
+          obj
+        end
+      else
+        {}
+      end
+    end
+
+    attribute :enabled_tags do
+      object.tags.map(&:name)
+    end
+
+    attribute :tags_and_suggestions do
+      Tag.tags_and_suggestions_for(object, limit: 20)
+         .map(&:name)
+    end
+
+    attribute :meta_image do
+      image_uploader(object.meta_image)
+    end
+
+    attribute :path_segment do
+      localized_attribute(object, :path_segment)
+    end
+
+    attribute :ancestors do
+      object.ancestors.map do |p|
+        { id: p.id,
+          name: localized_attribute(p, :name),
+          path_segment: localized_attribute(p, :path_segment) }
+      end
+    end
+
     private
 
-    def localized_attribute(attr)
-      object.locales.index_with do |locale|
-        object.localize(locale).send(attr)
+    def image_uploader(image)
+      return { src: nil, image: nil } unless image
+
+      { src: dynamic_image_path(image, size: "500x"),
+        image: ::Admin::ImageResource.new(image).to_hash }
+    end
+
+    def localized_objects
+      object.locales.map { |l| object.localize(l) }
+    end
+
+    def localized_attribute(record, attr)
+      record.locales.index_with do |locale|
+        record.localize(locale).send(attr)
       end
     end
   end
