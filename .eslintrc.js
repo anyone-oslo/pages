f071d70b4862d9d485b8aa5d5762e7412294ba03
Inge Jørgensen <inge@elektronaut.no>
2023-05-11 17:53:03 +0200

    Configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,36 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "overrides": [
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  overrides: [
     {
-      "files": ["**/*.+(ts|tsx)"],
-      "parser": "@typescript-eslint/parser",
-      "extends": [
+      files: ["**/*.+(ts|tsx)"],
+      parser: "@typescript-eslint/parser",
+      extends: [
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
       ]
     }
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "project": true,
-    "sourceType": "module",
-    "tsconfigRootDir": __dirname
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: true,
+    sourceType: "module",
+    tsconfigRootDir: __dirname
   },
-  "plugins": [
-    "react", "react-hooks", "@typescript-eslint"
-  ],
-  "root": true,
-  "rules": {
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  root: true,
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
   },
-  "settings": {
-    "react": {
-      "version": "detect"
+  settings: {
+    react: {
+      version: "detect"
     }
   }
 };
