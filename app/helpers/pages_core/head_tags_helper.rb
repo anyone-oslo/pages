7f668d67c37447eece7a30c374a450a6fb41c25c
Inge Jørgensen <inge@elektronaut.no>
2023-08-02 14:23:08 +0200

    Don't include doctype/html opening tag in head_tag helper

--- app/helpers/pages_core/head_tags_helper.rb
+++ app/helpers/pages_core/head_tags_helper.rb
@@ -54,8 +54,7 @@ module PagesCore
       # The block output must be captured first
       block_output = block_given? ? capture(&block) : nil
 
-      safe_join(["<!doctype html>\n<html lang=\"#{I18n.locale}\">".html_safe,
-                 tag.head { safe_join(head_tag_contents(block_output), "\n") }])
+      tag.head { safe_join(head_tag_contents(block_output), "\n") }
     end
 
     # Generates a link to an RSS feed.
