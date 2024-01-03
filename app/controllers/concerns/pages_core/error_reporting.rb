5c60c6f528912f72df6524f920378d733ce7bb37
Inge Jørgensen <inge@elektronaut.no>
2024-01-03 14:41:35 +0100

    Update Sentry integration

--- app/controllers/concerns/pages_core/error_reporting.rb
+++ app/controllers/concerns/pages_core/error_reporting.rb
@@ -5,34 +5,26 @@ module PagesCore
     extend ActiveSupport::Concern
 
     included do
-      before_action :configure_sentry_context
+      before_action :configure_sentry_scope
     end
 
     protected
 
-    def configure_sentry_context
-      return if Rails.env.test?
+    def configure_sentry_scope
+      return if Rails.env.test? || !Object.const_defined?("Sentry")
 
-      if Object.const_defined?("Sentry")
-        Sentry.set_user(current_user_context)
-        Sentry.set_tags(locale: params[:locale] || I18n.default_locale.to_s)
-        Sentry.set_extras(params: params.to_unsafe_h)
-      elsif Object.const_defined?("Raven")
-        configure_legacy_sentry_context
+      Sentry.configure_scope do |scope|
+        scope.set_context("params", params.to_unsafe_h)
+        scope.set_tags(locale: params[:locale] || I18n.default_locale.to_s)
+        scope.set_user(current_user_context)
       end
     end
 
-    def configure_legacy_sentry_context
-      Raven.user_context(current_user_context)
-      Raven.tags_context(locale: params[:locale] || I18n.default_locale.to_s)
-      Raven.extra_context(params: params.to_unsafe_h)
-    end
-
     def current_user_context
-      return { user_id: :guest } unless logged_in?
+      return {} unless logged_in?
 
-      { user_id: current_user.id,
-        user_email: current_user.email }
+      { id: current_user.id,
+        email: current_user.email }
     end
   end
 end
