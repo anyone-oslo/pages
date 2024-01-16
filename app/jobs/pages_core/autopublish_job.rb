93bdf6308df2e577486fd6e2c4035a28a4ca4937
Inge Jørgensen <inge@elektronaut.no>
2024-01-16 18:06:40 +0100

    AutopublishJob retry

--- app/jobs/pages_core/autopublish_job.rb
+++ app/jobs/pages_core/autopublish_job.rb
@@ -4,6 +4,8 @@ module PagesCore
   class AutopublishJob < ApplicationJob
     queue_as :pages_core
 
+    retry_on StandardError, attempts: 10, wait: :polynomially_longer
+
     def perform
       Autopublisher.run!
     end
