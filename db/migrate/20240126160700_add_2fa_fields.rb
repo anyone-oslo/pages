e42af67e1b80eb570a84b586c305910f26145451
Inge Jørgensen <inge@elektronaut.no>
2024-02-02 15:14:53 +0100

    Safe migration

--- db/migrate/20240126160700_add_2fa_fields.rb
+++ db/migrate/20240126160700_add_2fa_fields.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Add2faFields < ActiveRecord::Migration[7.0]
+  class User < ApplicationRecord; end
+
   def change
     change_table :users do |t|
       t.boolean :otp_enabled, null: false, default: false
@@ -14,7 +16,9 @@ class Add2faFields < ActiveRecord::Migration[7.0]
 
     reversible do |dir|
       dir.up do
-        User.find_each { |u| u.update(session_token: SecureRandom.hex(32)) }
+        User.find_each do |u|
+          u.update_columns(session_token: SecureRandom.hex(32))
+        end
         change_column_null :users, :session_token, false
       end
     end
