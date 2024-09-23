703acdf02df01b5f89c4f499e8e291f3617e630a
Inge Jørgensen <inge@elektronaut.no>
2024-09-23 13:22:11 +0200

    Do not include redirecting pages in sitemap

--- app/controllers/pages_core/sitemaps_controller.rb
+++ app/controllers/pages_core/sitemaps_controller.rb
@@ -16,27 +16,29 @@ module PagesCore
     end
 
     def pages
-      respond_to do |format|
-        format.xml { render xml: pages_sitemap.to_xml }
+      render_sitemap do |map|
+        Page.published.where.not(skip_index: true)
+            .localized(content_locale).find_each do |page|
+          next if page.redirects?
+
+          map.add(page_url(page.locale, page), lastmod: page.updated_at)
+        end
       end
     end
 
     private
-
-    def pages_sitemap
-      pages = Page.published.where.not(skip_index: true)
-                  .localized(content_locale)
-
-      PagesCore::Sitemap.new do |map|
-        pages.each { |p| map.add(page_url(p.locale, p), lastmod: p.updated_at) }
-      end
-    end
 
     def locales
       if PagesCore.config.locales
         PagesCore.config.locales.keys
       else
         [I18n.default_locale]
+      end
+    end
+
+    def render_sitemap(&)
+      respond_to do |format|
+        format.xml { render xml: PagesCore::Sitemap.new(&).to_xml }
       end
     end
   end
