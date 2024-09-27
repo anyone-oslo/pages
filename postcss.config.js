bec6465954f1f12bbd79aed23ed0876062072968
Inge Jørgensen <inge@elektronaut.no>
2024-09-27 12:52:26 +0200

    Switch to postcss-image-inliner

--- postcss.config.js
+++ postcss.config.js
@@ -2,17 +2,15 @@ module.exports = {
   plugins: [
     require("postcss-import-ext-glob"),
     require("postcss-import"),
+    require("postcss-image-inliner")({ assetsPaths: ["../images"] }),
     require("postcss-mixins"),
-    require("postcss-url")(
-      {
-        filter: /webfonts\/fa-/,
-        url: "copy",
-        basePath: "../../../../node_modules/@fortawesome/fontawesome-free/css",
-        assetsPath: "../fonts",
-        useHash: true
-      },
-      { filter: "**/*.svg", url: "inline", basePath: "../images" }
-    ),
+    require("postcss-url")({
+      filter: /webfonts\/fa-/,
+      url: "copy",
+      basePath: "../../../../node_modules/@fortawesome/fontawesome-free/css",
+      assetsPath: "../fonts",
+      useHash: true
+    }),
     require("postcss-preset-env")({ stage: 1 }),
     require("postcss-calc")({ precision: 10 }),
     require("autoprefixer"),
